05a9ccd0badebe886ccb24ab6a35add538020f93
unixgeekdias <135957077+unixgeekdias@users.noreply.github.com>
2023-06-08 13:26:28 +0500

    Добавил код калькулятора

---
+++ calc.py
@@ -0,0 +1,20 @@
+import sys
+
+def calc():
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sys.argv[2] == "+":
+        print(a+b)
+
+    if sys.argv[2] == "-":
+        print(a-b)
+
+    if sys.argv[2] == "*":
+        print(a*b)
+
+    if sys.argv[2] == "/":
+        print(a/b)
+
+
+calc()
